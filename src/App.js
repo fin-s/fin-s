93e88668edcf978cd69ac8fd073deb8e64e4b9b2
Jake Hyer <jakehyer@Jakes-MacBook-Air.local>
2019-06-03 15:37:32 -0600

    debts, income, expenses, registerWizzard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import RegisterWizzard from './Components/RegisterWizzard'
 
 function App() {
   return (
     <div className="App">
-      
+      <RegisterWizzard />
     </div>
   );
 }
