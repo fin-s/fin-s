deabe18e97f1d7a8c3f0c405ef5cd3d08e762cfb
Tyler Sumsion <tylersumsion@Tylers-MacBook.local>
2019-06-03 11:37:45 -0600

    made login and register files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import LoginForm from './Components/LoginRegister/LoginForm'
+import Register from './Components/LoginRegister/Register'
 
 function App() {
   return (
     <div className="App">
-      
+      <LoginForm />
+      <Register />
     </div>
   );
 }
