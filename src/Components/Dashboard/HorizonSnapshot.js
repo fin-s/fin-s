91d338fea5799cb6659d4558416492d3cbaf55d2
Tyler Sumsion <tylersumsion@Tylers-MacBook.local>
2019-06-06 11:34:17 -0600

    added app description

--- src/Components/Dashboard/HorizonSnapshot.js
+++ src/Components/Dashboard/HorizonSnapshot.js
@@ -2,39 +2,29 @@ import React from 'react'
 import Lottie from 'react-lottie'
 import animationData from '../../Lotties/animation-w360-h240.json'
 
-function HorizonSnapshot(props){
+function HorizonSnapshot(props) {
   const defaultOptions = {
     loop: true,
     autoplay: true,
     animationData: animationData,
     resizeMode: 'cover'
   }
-  if(props.loadingSnapshot){
+  if (props.loadingSnapshot) {
     return (
-      <div>
+      <div className='horizon'>
         {/* <h3>Horizon Snapshot</h3>
         <h4>Loading...</h4> */}
-        <Lottie options={defaultOptions}/>
+        <h2 className='title'>horizon snapshot</h2>
+        <Lottie options={defaultOptions} />
       </div>
     )
   } else {
     let showList = []
-    {
-      props.snapshotList.map(element => {
-        element.dueDates.map(date => {
-          let balance 
-          if(date.balance){
-            balance = `Remaining balance: $${date.balance}`
-          } else {
-            balance = ''
-          }
-          showList.push(`${element.month} ${date.dueDate}: ${date.nickname}  $${date.amount}.  ${balance}`)
-        })
-      })
-    }
-      return (
-        <div>
-          <h3>Horizon Snapshot</h3>
+
+    return (
+      <>
+        <h2 className='title'>horizon snapshot</h2>
+        <div className='innerInfo'>
           {/* {props.snapshotList.map((ele) => {
             return (
               <section>
@@ -42,11 +32,32 @@ function HorizonSnapshot(props){
               </section>
             )
           })} */}
-          <ul>{showList.map(element => {
-          return <li>{element}</li>
-          })}</ul>
+          {/* <ul>{showList.map(element => {
+            return <li>{element}</li>
+          })}</ul> */}
+
+          {
+            props.snapshotList.map(element => {
+              element.dueDates.map(date => {
+                let balance
+                if (date.balance) {
+                  balance = `Remaining balance: $${date.balance}`
+                } else {
+                  balance = ''
+                }
+                // showList.push(`${<span className='span'>{element.month}</span>} ${date.dueDate}: ${date.nickname}  $${date.amount}.  ${balance}`)
+                return <div><span>{element.month}</span>
+                  <span>{date.dueDate}: </span>
+                  <p>{date.nickname}</p>
+                  <span>{date.amount}</span>
+                  <p>{balance}</p>
+                </div>
+              })
+            })
+          }
         </div>
-      )
+      </>
+    )
   }
 }
 export default HorizonSnapshot
