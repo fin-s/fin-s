0cb6d5754e0791e6f6d1c2a862626aa9930e7447
Tyler Sumsion <tylersumsion@Tylers-MacBook.local>
2019-06-07 14:57:01 -0600

    working on style for wizard

--- src/Components/Expenses.js
+++ src/Components/Expenses.js
@@ -33,15 +33,15 @@ class Expenses extends Component {
   render(){
     return(
       <div>
-        <input onChange={this.handleChange} name='nickname' placeholder='Expense name' />
-        <input onChange={this.handleChange} name='amount' placeholder='Amount'/>
+        <input onChange={this.handleChange} name='nickname' placeholder='expense name' type='text'/>
+        <input onChange={this.handleChange} name='amount' placeholder='amount' type='text'/>
         <textarea onChange={this.handleChange} columns={20} rows={5} name='notes' placeholder='notes' />
-        <input type='number' min='1' max='28' placeholder='Day of the Month this comes due' onChange={this.handleChange} name='dueDate' />
-        <button onClick={this.confirmExpense}>Add Expense</button>
+        <input type='number' min='1' max='28' placeholder='day' onChange={this.handleChange} name='dueDate' />
+        <button onClick={this.confirmExpense} type="button" class="btn btn-outline-secondary" >add expense</button>
         {this.props.expenses.map((current, index) => {
           return <span key={index}>{current.nickname} {current.amount}</span>
         })}
-        <span onClick={()=>this.props.history.push('/dashboard')}>Skip for now</span>
+        <span onClick={()=>this.props.history.push('/dashboard')}>skip for now</span>
       </div>
     )
   }
