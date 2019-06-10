ed6ec3364fe8053d0b73730a13c6e74eef13a1ed
Jake Hyer <jakehyer@Jakes-MacBook-Air.local>
2019-06-10 15:45:25 -0600

    got the slider working

---
+++ src/Components/Dashboard/Handle.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function Handle({ // your handle component
+  handle: { id, value, percent }, 
+  getHandleProps
+}) {
+  return (
+    <div
+      style={{
+        left: `${percent}%`,
+        position: 'absolute',
+        marginLeft: -15,
+        marginTop: 25,
+        zIndex: 2,
+        width: 30,
+        height: 30,
+        border: 0,
+        textAlign: 'center',
+        cursor: 'pointer',
+        borderRadius: '50%',
+        backgroundColor: '#B3A97A',
+        color: '#333',
+      }}
+      {...getHandleProps(id)}
+    >
+      <div style={{ fontFamily: 'Roboto', fontSize: 11, marginTop: -35 }}>
+        {value}
+      </div>
+    </div>
+  )
+}
