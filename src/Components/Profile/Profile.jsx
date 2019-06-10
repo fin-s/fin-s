87caca16e22f35c390d5bcdf7dc819df8aaba114
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-06-10 15:28:31 -0600

    Profile mostly working

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -24,15 +24,21 @@ class Profile extends Component {
   }
 
   async componentDidMount() {
+    this.fetchUserInfo()
+  }
+
+  fetchUserInfo = async () => {
     let user = await axios.get('/api/users')
     // console.log(user.data)
     this.setState({
       incomes: user.data.incomes,
       debts: user.data.debts,
       expenses: user.data.expenses,
-      loading: false
+      loading: false,
+      addDebt: false,
+      addExpense: false,
+      addIncome: false
     })
-
   }
 
   toggleAddIncome = () => {
@@ -60,6 +66,27 @@ class Profile extends Component {
     })
   }
 
+  handleAddIncome = async (e) => {
+    e.preventDefault()
+    const income = {
+      nickname: this.state.incomeNickname,
+      type: this.state.incomeFrequency,
+      amount: +this.state.incomeAmount,
+      notes: this.state.incomeNotes,
+      interval: {
+        frequency: this.state.incomeFrequency,
+        incomeDate1: +this.state.incomeDate1,
+        incomeDate2: +this.state.incomeDate2,
+        incomeWeekday: +this.state.incomeWeekday
+      }
+    }
+    this.setState({
+      loading: true
+    })
+    await axios.post('/api/list/incomes', {incomes: [income]})
+    this.fetchUserInfo()
+  }
+
 
   render() {
     return (
@@ -74,47 +101,49 @@ class Profile extends Component {
                   <FontAwesomeIcon icon='plus-circle' />
                   <p>Add new income</p>
                 </div>
-                {this.state.addIncome ? <form className='add-form'>
-                  <p>Income name</p>
-                  <input onChange={(e) => this.handleChange(e)} name='incomeNickname' type="text" />
-                  <p>Income frequency</p>
-                  <select onChange={(e) => this.handleChange(e)} name="incomeFrequency">
-                    <option value="">--Income Frequency--</option>
-                    <option value="monthly">Monthly</option>
-                    <option value="semi-monthly">Semi-monthly</option>
-                    <option value="weekly">Weekly</option>
-                  </select>
-                  {this.state.incomeFrequency === 'monthly' ?
-                    <div>
-                      <p>Payday</p>
-                      <input onChange={(e) => this.handleChange(e)} name='incomeDate1' type="number" />
-                    </div> : this.state.incomeFrequency === 'semi-monthly' ?
-                      <div>
-                        <p>Paydays</p>
-                        <input onChange={(e) => this.handleChange(e)} name='incomeDate1' type="number" />
-                        <input onChange={(e) => this.handleChange(e)} name='incomeDate2' type="number" />
-                      </div> : this.state.incomeFrequency === 'weekly' ?
+                {this.state.addIncome ?
+                  <form onSubmit={(e)=> this.handleAddIncome(e)} className='add-form'>
+                    <p>Income name</p>
+                    <input onChange={(e) => this.handleChange(e)} name='incomeNickname' type="text" />
+                    <p>Income frequency</p>
+                    <select onChange={(e) => this.handleChange(e)} name="incomeFrequency">
+                      <option value="">--Income Frequency--</option>
+                      <option value="monthly">Monthly</option>
+                      <option value="semi-monthly">Semi-monthly</option>
+                      <option value="weekly">Weekly</option>
+                    </select>
+                    {this.state.incomeFrequency === 'monthly' ?
                       <div>
                         <p>Payday</p>
-                        <select onChange={(e) => this.handleChange(e)} name="incomeWeekday">
-                          <option value={0}>Sunday</option>
-                          <option value={1}>Monday</option>
-                          <option value={2}>Tuesday</option>
-                          <option value={3}>Wednesday</option>
-                          <option value={4}>Thursday</option>
-                          <option value={5}>Friday</option>
-                          <option value={6}>Saturday</option>
-                        </select>
-                      </div> : <p>--Select an income frequency--</p>}
-                  <p>Amount</p>
-                  <input onChange={(e) => this.handleChange(e)} name='incomeAmount' type="number" />
-                  <p>Notes</p>
-                  <input onChange={(e) => this.handleChange(e)} type="text" name='incomeNotes' />
+                        <input onChange={(e) => this.handleChange(e)} name='incomeDate1' type="number" />
+                      </div> : this.state.incomeFrequency === 'semi-monthly' ?
+                        <div>
+                          <p>Paydays</p>
+                          <input onChange={(e) => this.handleChange(e)} name='incomeDate1' type="number" />
+                          <input onChange={(e) => this.handleChange(e)} name='incomeDate2' type="number" />
+                        </div> : this.state.incomeFrequency === 'weekly' ?
+                          <div>
+                            <p>Payday</p>
+                            <select onChange={(e) => this.handleChange(e)} name="incomeWeekday">
+                              <option value={0}>Sunday</option>
+                              <option value={1}>Monday</option>
+                              <option value={2}>Tuesday</option>
+                              <option value={3}>Wednesday</option>
+                              <option value={4}>Thursday</option>
+                              <option value={5}>Friday</option>
+                              <option value={6}>Saturday</option>
+                            </select>
+                          </div> : <p>--Select an income frequency--</p>}
+                    <p>Amount</p>
+                    <input onChange={(e) => this.handleChange(e)} name='incomeAmount' type="number" />
+                    <p>Notes</p>
+                    <input onChange={(e) => this.handleChange(e)} type="text" name='incomeNotes' />
+                    <button type='submit'>Submit</button>
 
-                </form> : <></>}
+                  </form> : <></>}
 
                 {this.state.incomes.map(element => {
-                  return <ProfileIncome key={element._id} income={element}/>
+                  return <ProfileIncome key={element._id} income={element} />
                 })}
               </section>
               <section className="column debts">
