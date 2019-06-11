14f894099f5ebb0d5bead399f2f03b8ee202ed3e
Alex Doss <alexdoss@Alexs-MacBook-Pro-5.local>
2019-06-11 15:13:56 -0600

    I need to pull down the formatting fixes

--- src/Components/Dashboard/StepsProgressBar.js
+++ src/Components/Dashboard/StepsProgressBar.js
@@ -4,25 +4,31 @@ class ProgressBar extends Component {
   constructor(props){
     super(props)
 
+    this.state = {
+      progressWidth: this.props.stepsCompleted.reduce( (accumulator, currentValue, currentIndex, array) => accumulator + currentValue )
+    }
   }
 
   render() {
-    
+    console.log(this.props.stepsCompleted)
+    console.log(this.state.progressWidth)
     return (
       <div
       className='outerProgressBar'
         style={{
           border: '1px gold solid',
-          height: '5%',
+          height: '15px',
           width: '96%',
-          margin: '1% 2%'
+          margin: '1% 2%',
+          borderRadius: '15px'
           }}>
         <div
         className='Progress'
         style={{
           backgroundColor: 'gold',
           height: '100%',
-          width: '50%'
+          width: '50%',
+          borderRadius: '15px'
         }}></div>
       </div>
     )
