973110ee9ea70215a9993e173f3a09a07c09bb24
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-06-12 15:44:56 -0600

    Andrew tests

--- tests/andrew.test.js
+++ tests/andrew.test.js
@@ -1,82 +1,23 @@
-const { getList } = require('../server/controllers/getList')
+const andrewFunc = require('../src/utils/andrew.functions')
 
-
-
-
-const incomes = [
-    {
-        "interval": {
-            "frequency": "monthly",
-            "incomeDate1": 5,
-            "incomeDate2": null,
-            "incomeWeekday": null
-        },
-        "_id": "5cf805c47033a844c89d3d4f",
-        "nickname": "monthly",
-        "type": "monthly",
-        "amount": 500,
-        "notes": "hello"
-    },
-    {
-        "interval": {
-            "frequency": "weekly",
-            "incomeDate1": null,
-            "incomeDate2": null,
-            "incomeWeekday": 4
-        },
-        "_id": "5cf805c47033a844c89d3d50",
-        "nickname": "weekly",
-        "type": "monthly",
-        "amount": 500,
-        "notes": "hello"
-    }
-]
-const debts = [
-    {
-        "_id": "5cf805c57033a844c89d3d53",
-        "nickname": "Credit card 1",
-        "balance": 1500,
-        "interestRate": 2930,
-        "dueDate": 15,
-        "minimumPayment": 50,
-        "actualPayment": 100,
-        "notes": "note 1"
-    },
-    {
-        "_id": "5cf805c57033a844c89d3d54",
-        "nickname": "Credit card 2",
-        "balance": 3000,
-        "interestRate": 1529,
-        "dueDate": 10,
-        "minimumPayment": 100,
-        "actualPayment": 500,
-        "notes": "note 1"
-    }
-]
-const expenses = [
-    {
-        "_id": "5cf805c57033a844c89d3d57",
-        "nickname": "Water",
-        "dueDate": 7,
-        "amount": 20,
-        "notes": "note 2"
-    },
-    {
-        "_id": "5cf805c57033a844c89d3d58",
-        "nickname": "Electricity",
-        "dueDate": 10,
-        "amount": 100,
-        "notes": "note 2"
-    }
-]
-
-test('Array length should be 3', () => {
-    let list = getList(incomes, debts, expenses)
-    expect(list.length).toBe(3)
+test('/auth/login should return user object', () => {
+    expect(andrewFunc.axiosPost('http://localhost:8989/auth/login', { email: 'h', password: 'h' })).resolves.toEqual({"__v": 0, "_id": "5d0162087bbe861020e087f5", "debts": [{"_id": "5d01624d7bbe861020e087fa", "actualPayment": 100, "balance": 1500, "dueDate": 15, "interestRate": 29.3, "minimumPayment": 50, "nickname": "Credit card 1", "notes": "note 1"}, {"_id": "5d01624d7bbe861020e087fb", "actualPayment": 500, "balance": 3000, "dueDate": 10, "interestRate": 15.29, "minimumPayment": 100, "nickname": "Credit card 2", "notes": "note 1"}], "email": "h", "expenses": [{"_id": "5d01624d7bbe861020e087fe", "amount": 20, "dueDate": 7, "nickname": "Water", "notes": "note 2"}, {"_id": "5d01624d7bbe861020e087ff", "amount": 100, "dueDate": 10, "nickname": "Electricity", "notes": "note 2"}], "firstName": "Test", "incomes": [{"_id": "5d01624d7bbe861020e087f6", "amount": 500, "interval": {"frequency": "monthly", "incomeDate1": 5, "incomeDate2": null, "incomeWeekday": null}, "nickname": "monthly", "notes": "hello", "type": "monthly"}, {"_id": "5d01624d7bbe861020e087f7", "amount": 500, "interval": {"frequency": "weekly", "incomeDate1": null, "incomeDate2": null, "incomeWeekday": 4}, "nickname": "weekly", "notes": "hello", "type": "monthly"}], "lastName": "Test"})
 })
 
-test('Array length should not be 2 or 4', () => {
-    let list = getList(incomes, debts, expenses)
-    expect(list.length).not.toBe(2)
-    expect(list.length).not.toBe(4)
+test('Redirect if not logged in when attempting to fetch list', ()=> {
+    expect(andrewFunc.axiosGet('http://localhost:8989/api/list'))
+    .resolves.toBe('User not logged in')
+})
+
+test('Get three steps should return 3 steps', () => {
+    expect(andrewFunc.getThreeSteps().length).toBe(3)
+})
+
+test('Get three steps should return the correct 3 steps', () => {
+    expect(andrewFunc.getThreeSteps()).toContain(1,4,6)
+})
+
+test('Get step text returns the correct steps', () => {
+    expect(andrewFunc.getStepText()).toContain("Set all payments on auto-pay to avoid late fees and penalties", "Set all debt payments to the minimum except the smallest", "Once you have paid off the smallest debt roll that payment to the next smallest debt"
+    )
 })
