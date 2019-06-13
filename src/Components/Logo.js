984fbf2f8a00e5293816e39d9773a6097d6e80e7
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-06-13 15:16:28 -0600

    Fixed errors

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -5,7 +5,7 @@ import logo from '../Images/newFinS_logo.png'
 
 function Logo() {
   return (
-    <div className='navBarLinks' style={{paddingTop: '15px', marginBottom: '15px'}}>
+    <div className='navBarLinks' style={{paddingTop: '20px'}}>
       <Link to ='/dashboard'>
         <img className='nav-logo'
         src = {logo} 
